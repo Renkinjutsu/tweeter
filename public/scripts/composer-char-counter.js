3e424bb7bf47b70becadfc98fc4c4cf57b11a2cf
Renkinjutsu <lucas.cheng12@gmail.com>
2019-05-14 17:59:29 -0400

    changed character counter html to text

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
     $("textarea#tweetbox").on("input", function() {
         $(this).val().length >= 140 ?
         $("span.counter").attr("style", "color:red") : $("span.counter").attr("style", "color:black")
-        return $("span.counter").html(140 - $(this).val().length);    
+        return $("span.counter").text(140 - $(this).val().length);    
   })
 });
